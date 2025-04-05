c6388e6dd1177072e74099dcb03f2981242bee2a
ogslbevey <veyracb@ogsl.ca>
2025-04-04 20:51:43 -0700

    feat(harvester): remove external requests for platform mapping
    - Add pre-generated 'platforms_nerc_ioos.csv' to the Docker image
    - Load platform mapping directly from local CSV
    - Remove dynamic HTTP requests to NERC and MMISW

---
+++ harvester/fetch_platforms_data.py
@@ -0,0 +1,70 @@
+# script: fetch_platforms_data.py
+import pandas as pd
+import requests
+
+def get_l06_codes_and_labels():
+    url = "http://vocab.nerc.ac.uk/collection/L06/current/?_profile=nvs&_mediatype=application/ld+json"
+    platforms = requests.get(url).json()["@graph"]
+    platforms_parsed = {}
+    l06Lookup = {}
+
+    for platform in platforms:
+        if "identifier" not in platform:
+            continue
+        label = platform.get("skos:prefLabel", {}).get("@value")
+        if not label:
+            continue
+        broader = platform.get("skos:broader", [])
+        id = platform["@id"]
+        found_parent_platform = False
+        for url in broader:
+            if "L06" in url["@id"]:
+                platforms_parsed[id] = {"broader_L06_url": url["@id"], "l06_label": label}
+                found_parent_platform = True
+        if not found_parent_platform:
+            platforms_parsed[id] = {"broader_L06_url": id, "l06_label": label}
+        l06Lookup[id] = label
+
+    for l06_url_code, item in platforms_parsed.items():
+        broaderL06 = item["broader_L06_url"]
+        res = l06Lookup[broaderL06]
+        platforms_parsed[l06_url_code]["category"] = res
+    df = pd.DataFrame.from_dict(platforms_parsed, orient="index")
+    if "broader_L06_url" in df.columns:
+        del df["broader_L06_url"]
+    df.index = df.index.astype(str).str.split("/").str[-2]  # 🔧 Correction ici
+    df.index.names = ["l06_code"]
+    return df
+
+def get_ioos_to_l06_mapping():
+    url = "https://mmisw.org/ont/api/v0/ont?format=jsonld&iri=http://mmisw.org/ont/bodc/MapSeaVoxPlatforms2IOOSandRDIPlatforms"
+    res = requests.get(url).json()
+    rows = []
+    for k in res["@graph"]:
+        try:
+            predicate = k.get("predicate", "").split(":")[-1]
+            ioos_code = k.get("subject", "").split("/")[-1]
+            nerc_l06_code = k.get("object", "").split("/")[-2]
+
+            if ioos_code and nerc_l06_code:
+                rows += [
+                    {"ioos_label": ioos_code, "predicate": predicate, "l06_code": nerc_l06_code}
+                ]
+        except IndexError:
+            pass
+
+    df = pd.DataFrame(rows)
+    preference_list = ["exactMatch", "narrowMatch", "broadMatch", "relatedMatch"]
+    df["Pref"] = pd.Categorical(df["predicate"], categories=preference_list, ordered=True)
+    df = df.sort_values(["ioos_label", "Pref"]).drop_duplicates("ioos_label")
+    df.drop(["Pref", "predicate"], axis=1, inplace=True)
+    df.reset_index(inplace=True, drop=True)
+    df = df.set_index("l06_code")
+    return df
+
+l06_df = get_l06_codes_and_labels()
+ioos_df = get_ioos_to_l06_mapping()
+
+platforms_nerc_ioos = l06_df.join(ioos_df).reset_index().fillna("")
+
+platforms_nerc_ioos.to_csv("platforms_nerc_ioos.csv", index=False)
